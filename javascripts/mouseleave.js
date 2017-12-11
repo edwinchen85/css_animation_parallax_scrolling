c27dcc572e7f801cb378298f5343be54d15cd2e1
edwinchen85 <edwinchen85@gmail.com>
2017-12-12 00:44:00 +0800

    Use session storage to check modal has been opened to allow modal to open only once

--- javascripts/mouseleave.js
+++ javascripts/mouseleave.js
@@ -1,8 +1,9 @@
 $(function() {
 
   $(window).mouseleave(function(e) {
+    var modalSeen = sessionStorage.getItem('modalSeen');
     // if mouse has actually left the window
-    if (e.toElement == null) {
+    if (e.toElement == null && !modalSeen) {
       // document.documentElement references html tag
       document.documentElement.classList.add('mouse-out');
     }
@@ -11,6 +12,7 @@ $(function() {
   $('#close-modal').click(function(e) {
     e.preventDefault();
     document.documentElement.classList.remove('mouse-out');
+    sessionStorage.setItem('modalSeen', true);
   });
 
 });
